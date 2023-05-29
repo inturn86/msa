7a6ff4ed7640b33995a4ef6243d1363d7b8e8932
inturn86 <cjswoiie@naver.com>
2023-05-29 20:34:15 +0900

    [feat] modify repository

--- second-service/src/main/java/com/sdc/secondservice/service/SecondRepository.java
+++ second-service/src/main/java/com/sdc/secondservice/service/SecondRepository.java
@@ -1,9 +1,13 @@
 package com.sdc.secondservice.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Slf4j
+@RequiredArgsConstructor
 public class SecondRepository {
+
+//
 }
