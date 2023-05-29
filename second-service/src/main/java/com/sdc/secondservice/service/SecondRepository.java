5dc8303687a7cbac68a918aaa6fa0184414317a9
cjswoiie <cjswoiie@gmail.com>
2023-05-29 20:33:00 +0900

    [feat] SecondService modifyg

--- second-service/src/main/java/com/sdc/secondservice/service/SecondRepository.java
+++ second-service/src/main/java/com/sdc/secondservice/service/SecondRepository.java
@@ -1,7 +1,9 @@
 package com.sdc.secondservice.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Slf4j
 public class SecondRepository {
 }
