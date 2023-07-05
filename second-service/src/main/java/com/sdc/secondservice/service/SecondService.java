be8a52b39d177bee5b41812a9f54fe43844a9a94
inturn86 <cjswoiie@naver.com>
2023-07-05 13:03:35 +0900

    [feat] user history add 기능 수정.

--- second-service/src/main/java/com/sdc/secondservice/service/SecondService.java
+++ second-service/src/main/java/com/sdc/secondservice/service/SecondService.java
@@ -6,14 +6,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@Service
-@Slf4j
-@RequiredArgsConstructor
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class SecondService {
 
 	private final UserHistoryService userHistoryService;
