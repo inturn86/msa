73e39a63dac67e14d1eb4bbca17b7741ac8d5a46
inturn86 <cjswoiie@naver.com>
2023-07-05 12:37:39 +0900

    [feat] user add 기능 추가.

--- second-service/src/main/java/com/sdc/secondservice/service/SecondService.java
+++ second-service/src/main/java/com/sdc/secondservice/service/SecondService.java
@@ -1,4 +1,21 @@
 package com.sdc.secondservice.service;
 
+
+import com.sdc.secondservice.service.history.UserHistoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class SecondService {
+
+	private final UserHistoryService userHistoryService;
+
+	public void addUser(){
+		log.error("add User");
+		userHistoryService.addHistory();
+	}
+
 }
