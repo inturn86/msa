3dd526643ad7c8ea7561b3844c795f64aa42cf0b
cjswoiie <cjswoiie@gmail.com>
2023-07-05 12:40:40 +0900

    [feat] modify user 기능 추가.

--- second-service/src/main/java/com/sdc/secondservice/service/SecondService.java
+++ second-service/src/main/java/com/sdc/secondservice/service/SecondService.java
@@ -1,4 +1,13 @@
 package com.sdc.secondservice.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class SecondService {
+
+	public void modifyUser(){
+		log.error("modify User");
+	}
 }
